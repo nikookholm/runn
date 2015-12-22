1c432a26abd6f9d43db5b837ede8c37399a2bcd1
eggman1979 <kim.d.rasmussen@live.dk>
2015-12-22 22:36:56 +0100

    Ændret i ProfileCreation

--- app/src/main/java/s112011/runn/ProfileCreation.java
+++ app/src/main/java/s112011/runn/ProfileCreation.java
@@ -1,31 +1,41 @@
 package s112011.runn;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class ProfileCreation extends AppCompatActivity implements View.OnClickListener {
 
-    View root;
-
-
+    Button cancel, create;
 
     @Override
-    p
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        root  = inflater.inflate(R.layout.profile_creation, container, false);
-        return root;
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_creation_part_one);
 
+        cancel = (android.widget.Button) findViewById(R.id.button);
+        cancel.setOnClickListener(this);
+
+        create= (Button) findViewById(R.id.button2);
+        create.setOnClickListener(this);
+
+    }
 
     @Override
     public void onClick(View v) {
+
+            Intent intent = new Intent(this,MainActivity.class );
+            startActivity(intent);
+
+
 
     }
 }
