ad8bb6125d103958dd6a079d593259188419bd7d
eggman1979 <kim.d.rasmussen@live.dk>
2015-12-22 22:46:08 +0100

    Ændret i AgreementCreate

--- app/src/main/java/s112011/runn/AgreementCreate.java
+++ app/src/main/java/s112011/runn/AgreementCreate.java
@@ -1,29 +1,36 @@
 package s112011.runn;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class AgreementCreate extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class AgreementCreate extends Fragment implements View.OnClickListener {
+
+Button ok;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agreement_create);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.content_agreement_create, container, false);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ok = (Button) root.findViewById(R.id.button);
+        ok.setOnClickListener(this);
+
+
+        return root;
     }
 
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = new listAgreements();
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragmentView, fragment )
+                .addToBackStack(null)
+                .commit();
+    }
 }
