57a9228a4cc701c45f9627854a291016af8f9f31
nehfi <kim@aptget.dk>
2016-01-06 14:04:25 +0100

    Merge branch 'master' of /home/nehfi/AndroidStudioProjects/runn with conflicts.

---
+++ app/src/main/java/s112011/runn/CreateAgreement.java
@@ -0,0 +1,13 @@
+package s112011.runn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAgreement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_agreement);
+    }
+}
