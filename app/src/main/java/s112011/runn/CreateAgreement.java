e8bdfc8edf3836ad6ede54d78a86c4bfda45e148
nehfi <kim@aptget.dk>
2016-01-07 09:57:08 +0100

    Opdateret mainActivity, opret bruger og opret agreement

--- app/src/main/java/s112011/runn/CreateAgreement.java
+++ app/src/main/java/s112011/runn/CreateAgreement.java
@@ -2,12 +2,42 @@ package s112011.runn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CreateAgreement extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateAgreement extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_agreement);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerNiveau);
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Lav");
+        categories.add("Middel");
+        categories.add("Høj");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        spinner.setAdapter(dataAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
     }
 }
--- app/src/main/java/s112011/runn/MainActivity.java
+++ app/src/main/java/s112011/runn/MainActivity.java
@@ -1,5 +1,6 @@
 package s112011.runn;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-ImageButton search, createAppoint, settings;
 
+ImageButton search, createAppoint, settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,30 +21,33 @@ ImageButton search, createAppoint, settings;
         search = (ImageButton) findViewById(R.id.imageButton);
         search.setImageResource(R.drawable.ic_search_black_48dp);
 
-
         createAppoint = (ImageButton) findViewById(R.id.imageButton2);
         createAppoint.setImageResource(R.drawable.ic_add_black_48dp);
-        createAppoint.setElevation(2);
+        //createAppoint.setElevation(2);
         createAppoint.setOnClickListener(this);
-
 
         settings = (ImageButton) findViewById(R.id.imageButton3);
         settings.setImageResource(R.drawable.ic_settings_applications_black_48dp);
 
-        Fragment create =new listAgreements();
+        Fragment create = new listAgreements();
         getSupportFragmentManager().beginTransaction()
                .add(R.id.fragmentView, create )  // tom container i layout
                .commit();
 
     }
 
+    private void imageButtonOnClick(){
+        Intent intent = new Intent(MainActivity.this, CreateAgreement.class);
+        startActivity(intent);
+    }
+
     @Override
-    public void onClick(View v) {
-        if(v == createAppoint){
-            Fragment create =new AgreementCreate();
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragmentView, create)  // tom container i layout
-                    .commit();
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.imageButton2:
+                imageButtonOnClick();
+                break;
         }
     }
 }
