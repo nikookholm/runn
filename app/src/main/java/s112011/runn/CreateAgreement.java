2e98eca917bce1c887c35dff8d23623f641a19d7
nehfi <kim@aptget.dk>
2016-01-11 14:09:10 +0100

    CreateAgreement er ca færdig. har også lavet DAO og DTO til agreements

--- app/src/main/java/s112011/runn/CreateAgreement.java
+++ app/src/main/java/s112011/runn/CreateAgreement.java
@@ -1,5 +1,8 @@
 package s112011.runn;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,35 +11,97 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class CreateAgreement extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     MainActivity mainActivity = new MainActivity();
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
+    private int hour, minute;
+    Button btnDate, btnTime;
+    static final int DATE_PICKER_ID = 1111;
+    static final int TIME_PICKER_ID = 2222;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_agreement);
 
-
-
+        //Level Spinner
         Spinner spinner = (Spinner) findViewById(R.id.spinnerNiveau);
         spinner.setOnItemSelectedListener(this);
-
         List<String> categories = new ArrayList<String>();
         categories.add("Lav");
         categories.add("Middel");
         categories.add("Høj");
-
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
         spinner.setAdapter(dataAdapter);
 
+        calendar = Calendar.getInstance();
+
+        //DatePicker
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        btnDate = (Button) findViewById(R.id.buttonDate);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // On button click show datepicker dialog
+                showDialog(DATE_PICKER_ID);
+            }
+        });
+
+        //Timepicker
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        btnTime = (Button) findViewById(R.id.buttonTime);
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button click to show timepicker dialog
+                showDialog(TIME_PICKER_ID);
+            }
+        });
+
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_PICKER_ID:
+                return new DatePickerDialog(this, myDateListener, year, month, day);
+            case TIME_PICKER_ID:
+                return new TimePickerDialog(this, myTimeListener, hour, minute, true);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg_Y, int arg_M, int arg_D) {
+            btnDate.setText(new StringBuilder().append(arg_D).append("-").append(arg_M+1).append("-").append(arg_Y));
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener myTimeListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int arg_h, int arg_m) {
+            btnTime.setText(new StringBuilder().append(arg_h).append(":").append(arg_m));
+        }
+    };
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
