299ba9a983cf818296d74cb15c5596dc80a05d94
nehfi <kim@aptget.dk>
2016-01-21 10:24:53 +0100

    AgreementLists virker, ognu meden intetn

--- app/src/main/java/s112011/runn/AgreementsList.java
+++ app/src/main/java/s112011/runn/AgreementsList.java
@@ -1,10 +1,8 @@
 package s112011.runn;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,11 +13,9 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,10 +23,7 @@ import java.util.List;
  */
 public class AgreementsList extends AppCompatActivity {
 
-    //ImageView imgLoc, imgEvent, imgUser;
     List<AgreementDTO> agreements;
-    ProfileDAO profDAO;
-    ProfileDTO profDTO;
     ListView lVAll, lvOwn;
     TabHost tabHost;
     String userName;
@@ -48,7 +41,7 @@ public class AgreementsList extends AppCompatActivity {
         t.getHotAgreementsAsync(new DAOEvent() {
             @Override
             public void getHotAgreements(List<AgreementDTO> agreements) {
-                PopulateAgreementsList(agreements);
+                populateAgreementsList(agreements);
             }
         });
 
@@ -67,10 +60,7 @@ public class AgreementsList extends AppCompatActivity {
 
     }
 
-    //Alle agreementDAOer
-    //Alle ProfileDAOer
-
-    private void PopulateAgreementsList(List<AgreementDTO> ListAgreementsDTO){
+    private void populateAgreementsList(List<AgreementDTO> ListAgreementsDTO){
         agreements = ListAgreementsDTO;
         lVAll = (ListView) findViewById(R.id.listVAll);
         ArrayAdapter<AgreementDTO> adapter = new AgreementListAdapter();
@@ -78,9 +68,10 @@ public class AgreementsList extends AppCompatActivity {
         lVAll.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "OnItemClick", Toast.LENGTH_LONG).show();
-                //Intent intent = new Intent(getBaseContext(), ShowAgreement.class);
-                //startActivity(intent);
+                //Toast.makeText(getApplicationContext(), "OnItemClick", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getBaseContext(), ShowAgreement.class);
+                intent.putExtra("agreement", agreements.get(position));
+                startActivity(intent);
             }
         });
     }
@@ -88,18 +79,19 @@ public class AgreementsList extends AppCompatActivity {
     private class AgreementListAdapter extends ArrayAdapter<AgreementDTO> {
         ImageView imgLoc, imgEvent, imgUser;
         AgreementDTO currentAgreement;
+        ProfileDAO profDAO;
+        TextView tTitle;
+        TextView tLocation;
+        TextView tUser;
+        TextView tDate;
 
         public AgreementListAdapter() {
             super(AgreementsList.this, R.layout.agreement_item, agreements);
-
-            System.out.println(">>>>>>>>><<<<<<<<<<<< Super metode, agreementListAdapter");
         }
         @Override
         public View getView(int position, View view, ViewGroup viewGroup){
             if(view == null)
                 view = getLayoutInflater().inflate(R.layout.agreement_item, viewGroup, false);
-
-            System.out.println("-------------<<<<<<<<->>>>>>>>>> GETVIEW------------------------");
 
             imgEvent = (ImageView) view.findViewById(R.id.imgItemDate);
             imgEvent.setImageResource(R.drawable.ic_event_black_18dp);
@@ -109,47 +101,39 @@ public class AgreementsList extends AppCompatActivity {
             imgUser.setImageResource(R.drawable.ic_person_black_18dp);
 
             currentAgreement = agreements.get(position);
+            populateProfile(currentAgreement);
 
-            PopulateProfile(currentAgreement);
+            tTitle = (TextView) view.findViewById(R.id.textViewTitlePlaceholder);
+            tLocation = (TextView) view.findViewById(R.id.textViewLocationPlaceholder);
+            tUser = (TextView) view.findViewById(R.id.textViewUserPlaceholder);
+            tDate = (TextView) view.findViewById(R.id.textViewDatePlaceholder);
 
-            System.out.println("------------------------------<<<<<<<ID: " + currentAgreement.getCreatedBy());
-
-            TextView tTitle = (TextView) view.findViewById(R.id.textViewTitlePlaceholder);
-            TextView tLocation = (TextView) view.findViewById(R.id.textViewLocationPlaceholder);
-            TextView tUser = (TextView) view.findViewById(R.id.textViewUserPlaceholder);
-            TextView tDate = (TextView) view.findViewById(R.id.textViewDatePlaceholder);
-
-            tTitle.setText("" + currentAgreement.getTitle());
-            tLocation.setText("" + currentAgreement.getLocation());
-            tUser.setText("" + currentAgreement.getCreatedBy());
-            tDate.setText("" + currentAgreement.getTime());
-/*
-            tTitle.setText("TITELLØØØØB");
-            tLocation.setText("STEDDDETLØØB");
-            tUser.setText("MickeyMouseDenUberAwesomme");
-            tDate.setText("d24/8-2266");
-*/
             return view;
         }
 
-        private void PopulateProfile(AgreementDTO agreementDTO){
+        private void populateProfile(AgreementDTO agreementDTO){
             agreementDTO = currentAgreement;
-            System.out.println("!!!!!!!!!!!!!!!!!!!");
+            profDAO = new ProfileDAO();
+
             try {
-                profDAO.getProfileAsync(1, new DAOEvent(){
-                    //currentAgreement.getCreatedBy()
+                profDAO.getProfileAsync(currentAgreement.getCreatedBy(), new DAOEvent(){
+
                     @Override
                     public void getProfile(ProfileDTO profile) {
-                        System.out.println("################");
-                        //userName = profile.getUsername();
-                        //if(userName==null)
-                            //userName="FalskBrugernavn";
+                        userName = profile.getUsername();
+                        populateTextViews();
                     }
                 });
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("******************");
             }
+        }
+
+        private void populateTextViews(){
+            tTitle.setText("" + currentAgreement.getTitle());
+            tLocation.setText("" + currentAgreement.getLocation());
+            tUser.setText("" + userName);
+            tDate.setText("" + currentAgreement.getTime());
         }
     }
 
